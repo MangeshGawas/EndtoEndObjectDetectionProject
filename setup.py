971dc5148ae74fb2a8c50ead064d710e75eef046
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-05-29 14:36:02 +0530

    dataCollection code added

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'signLanguages',
     version= '0.0.0',
-    author= 'iNeuron',
-    author_email= 'boktiar@ineuron.ai',
+    author= 'mangesh',
+    author_email= 'manikgawas07@gmail.com',
     packages= find_packages(),
     install_requires = []
 
